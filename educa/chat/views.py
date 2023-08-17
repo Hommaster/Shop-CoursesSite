69303c74a27e76cf5e26bfbcebeda652c61d3a4b
Hommaster <ilya.pan.2017@mail.ru>
2023-08-17 20:43:21 +0300

    upgrade chat_system v1.5

--- educa/chat/views.py
+++ educa/chat/views.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseForbidden
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from courses.models import Module
 
 
 @login_required
@@ -10,3 +12,19 @@ def course_chat_room(request, course_id):
     except:
         return HttpResponseForbidden
     return render(request, 'chat/room.html', {'course': course})
+
+
+@login_required
+def module_chat_room(request, module_id):
+    try:
+        module = get_object_or_404(Module,
+                                   id=module_id,
+                                   course__students=request.user)
+    except:
+        return HttpResponseForbidden
+    return render(
+        request, 'chat/module_room.html',
+        {
+            'module': module,
+        }
+    )

8a813c3285fa474cf64f268f20782d26432ea828
Hommaster <ilya.pan.2017@mail.ru>
2023-08-29 19:37:20 +0300

    payment-system v1.2.3

--- educa/educa/settings.py
+++ educa/educa/settings.py
@@ -198,3 +198,17 @@ CHANNEL_LAYERS = {
         },
     },
 }
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'ilya.pan.@gmail.com'
+EMAIL_HOST_PASSWORD =''
+EMAIL_PORT = 1
+EMAIL_USE_TLS = True
+
+STRIPE_PUBLISHABLE_KEY = ''
+
+STRIPE_SECRET_KEY = ''
+
+STRIPE_API_VERSION = '2022-11-15'
+
+STRIPE_WEBHOOK_SECRET = ''

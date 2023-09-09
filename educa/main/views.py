1f8dcf32d5615db1a34f3ff225a20ab2397af248
Hommaster <ilya.pan.2017@mail.ru>
2023-09-10 00:23:12 +0300

    create redis-views system for courses

--- educa/main/views.py
+++ educa/main/views.py
@@ -8,3 +8,8 @@ from courses.models import Course
 class MainPage(ListView):
     template_name = 'main/main_page.html'
     model = Course
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['course'] = Course.objects.latest('-created')
+        return context

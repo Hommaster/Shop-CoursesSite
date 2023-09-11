e1bd5bfc8b3a663f9e92a0618625cd7e0dd22111
Hommaster <ilya.pan.2017@mail.ru>
2023-09-11 17:35:31 +0300

    create APIViews registrate

---
+++ educa/api/custom_permissions.py
@@ -0,0 +1,7 @@
+from rest_framework import permissions
+
+
+class IsNotAuthenticated(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+        return not request.user.is_authenticated
